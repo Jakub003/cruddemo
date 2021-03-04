8007b309a5184f1e3e899f424cc0d14aeddd99c1
Jakub <jakubmoz003@gmail.com>
2021-03-04 03:26:50 -0600

    added create and edit pages

---
+++ resources/views/kanbans/create.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('sidebar')
+    @include('components/titles')
+@endsection
+
+
+@section('content')
+
+    Create Page
+        
+@endsection
--- resources/views/kanbans/index.blade.php
+++ resources/views/kanbans/index.blade.php
@@ -6,8 +6,17 @@
 
 
 @section('content')
-
-    <h2 class="text-2xl font-semibold">All Projects at kanbans/index</h2>
+    <div class="flex space-x-4">
+        <h2 class="text-2xl font-semibold">All Projects at kanbans/index</h2>
+        <a href="#">
+            <div class="bg-green-300 hover:bg-green-400 font-semibold rounded h-8 py-1 px-3 text-white cursor-pointer">
+                <p>Create</p>
+            </div>
+        </a>
+        
+    </div>
+    {{-- {{ route('products.create') }} --}}
+    
 
     @if ($message = Session::get('success'))
         <div class="alert alert-success">
@@ -17,13 +26,17 @@
 
     @foreach ($kanbans as $kanban)
     
-    <div class="flex space-x-4">    
+    <div class="flex space-x-4">
+        {{-- Display --}}
         <div class="h-8 w-8 px-2 py-1 rounded {{ $kanban->color }} ">
             <i class=" text-base {{ $kanban->icon }} "></i>
         </div>     
         <h3 class="h-8 text-base font-semibold mt-1">{{ $kanban->title }}</h3>   
-
-
+        
+        {{-- Edit --}}
+        {{-- <a class="p-2 bg-green-500 text-white rounded" href="{{ route('products.edit',$kanban->id) }}"><i class=" bi bi-pencil-square "></i></a> --}}
+    
+        {{-- Delete --}}
         <form action="{{ route('kanbans.destroy',$kanban->id) }}" method="POST">
             @csrf
             @method('DELETE')
